d87903b66a2cae1482d3085872a762147eb44e69
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2017-11-18 00:09:01 +0100

    feat: firmware js/py version detection

--- src-sentinel/firmwares.py
+++ src-sentinel/firmwares.py
@@ -6,12 +6,14 @@ import zipfile
 import hashlib
 import json
 
-regex_homie = re.compile(
+REGEX_HOMIE = re.compile(
     b"\x25\x48\x4f\x4d\x49\x45\x5f\x45\x53\x50\x38\x32\x36\x36\x5f\x46\x57\x25")
-regex_name = re.compile(b"\xbf\x84\xe4\x13\x54(.+)\x93\x44\x6b\xa7\x75")
-regex_version = re.compile(b"\x6a\x3f\x3e\x0e\xe1(.+)\xb0\x30\x48\xd4\x1a")
-regex_brand = re.compile(b"\xfb\x2a\xf5\x68\xc0(.+)\x6e\x2f\x0f\xeb\x2d")
+REGEX_NAME = re.compile(b"\xbf\x84\xe4\x13\x54(.+)\x93\x44\x6b\xa7\x75")
+REGEX_VERSION = re.compile(b"\x6a\x3f\x3e\x0e\xe1(.+)\xb0\x30\x48\xd4\x1a")
+REGEX_BRAND = re.compile(b"\xfb\x2a\xf5\x68\xc0(.+)\x6e\x2f\x0f\xeb\x2d")
 
+REGEX_PYTHON_VERSION = r"^__version__ = ['\"]([^'\"]*)['\"]"
+REGEX_PYTHON_DESC = r"^\"\"\"([^(?:\"\"\")]*)\"\"\""
 
 def sizeof_fmt(num, suffix="B"):
     """Format file size in a human readable way"""
@@ -41,9 +43,9 @@ def scan_esp_firmware(path, fw_file, db):
 
     with open(os.path.join(path, fw_file), "rb") as firmware_file:
         firmware_binary = firmware_file.read()
-        regex_name_result = regex_name.search(firmware_binary)
-        regex_version_result = regex_version.search(firmware_binary)
-        regex_brand_result = regex_brand.search(firmware_binary)
+        regex_name_result = REGEX_NAME.search(firmware_binary)
+        regex_version_result = REGEX_VERSION.search(firmware_binary)
+        regex_brand_result = REGEX_BRAND.search(firmware_binary)
         if regex_name_result:
             db["name"] = regex_name_result.group(1).decode()
         if regex_version_result:
@@ -68,6 +70,16 @@ def scan_bundle_firmware(path, fw_file, db):
     with zipfile.ZipFile(fw_path, "r") as bundle:
         try:
             bundle.getinfo("main.py")
+            # version detection
+            main_as_text = bundle.read("main.py");
+            version_result = re.search(REGEX_PYTHON_VERSION, main_as_text, re.M)
+            if version_result:
+                db["version"] = version_result.group(1)
+
+            # description extraction
+            description_result = re.search(REGEX_PYTHON_DESC, main_as_text, re.M)
+            if description_result:
+                db["description"] = description_result.group(1)
         except Exception:
             pass
         else:
@@ -75,10 +87,9 @@ def scan_bundle_firmware(path, fw_file, db):
 
         try:
             bundle.getinfo("package.json")
-            # with bundle.read("package.json") as package_text:
-            #     logging.info(package_text)
-            #     package = json.loads(package_text)
-            #     db["version"] = package["version"]
+            package = json.loads(bundle.read('package.json'))
+            db["description"] = package["description"]
+            db["version"] = package["version"]
         except Exception:
             pass
         else:
